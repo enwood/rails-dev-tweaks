49b35bbc76f949b205640adde714b183e33ae01c
tgriffin <tech@enwood.ca>
2017-08-04 08:51:23 -0400

    Port fix for performance issue in Rails 4.1; Bump to 1.2.1
    https://github.com/wavii/rails-dev-tweaks/pull/24/commits/b0e5aec9939f0f
    ba0cfd3334ef151ee09b4d8d8d

--- lib/rails_dev_tweaks/granular_autoload/middleware.rb
+++ lib/rails_dev_tweaks/granular_autoload/middleware.rb
@@ -42,7 +42,7 @@ class RailsDevTweaks::GranularAutoload::Middleware
     application = Rails.application
 
     # Rails 3.2 defines reload_dependencies? and it only reloads if reload_dependencies? returns true.
-      (!application.class.method_defined?(:reload_dependencies?) ||
-        application.send(:reload_dependencies?))
+    # https://github.com/wavii/rails-dev-tweaks/pull/24/commits/b0e5aec9939f0fba0cfd3334ef151ee09b4d8d8d
+    application.config.reload_classes_only_on_change != true || application.reloaders.map(&:updated?).any?
   end
 end
